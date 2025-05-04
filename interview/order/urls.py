6b2b3db80146d995006038f297cbf7db2e0ba47d
Sara <1sarabyte@gmail.com>
2025-05-04 16:57:21 -0700

    Add url

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,10 +1,10 @@
 
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+from interview.order.views import OrderListCreateView, OrderTagListCreateView, DeactivateOrderView
 
 
 urlpatterns = [
     path('tags/', OrderTagListCreateView.as_view(), name='order-detail'),
+    path('orders/deactivate/<int:id>', DeactivateOrderView.as_view(), name='order-list'),
     path('', OrderListCreateView.as_view(), name='order-list'),
-
 ]
