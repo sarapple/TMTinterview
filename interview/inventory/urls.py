a893797170de340412dfa44227d44ca1a61bea0b
Sara <1sarabyte@gmail.com>
2025-05-04 16:53:58 -0700

    added to routes

--- interview/inventory/urls.py
+++ interview/inventory/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from interview.inventory.views import InventoryLanguageListCreateView, InventoryLanguageRetrieveUpdateDestroyView, InventoryListCreateView, InventoryRetrieveUpdateDestroyView, InventoryTagListCreateView, InventoryTagRetrieveUpdateDestroyView, InventoryTypeListCreateView, InventoryTypeRetrieveUpdateDestroyView
+from interview.inventory.views import InventoryLanguageListCreateView, InventoryLanguageRetrieveUpdateDestroyView, InventoryListCreateView, InventoryRetrieveUpdateDestroyView, InventoryTagListCreateView, InventoryTagRetrieveUpdateDestroyView, InventoryTypeListCreateView, InventoryTypeRetrieveUpdateDestroyView, InventoryListView
 from interview.order.views import OrderListCreateView, OrderTagListCreateView
 
 
@@ -12,5 +12,6 @@ urlpatterns = [
     path('languages/', InventoryLanguageListCreateView.as_view(), name='inventory-languages-list'),
     path('tags/', InventoryTagListCreateView.as_view(), name='inventory-tags-list'),
     path('types/', InventoryTypeListCreateView.as_view(), name='inventory-types-list'),
+    path('inventory/', InventoryListView.as_view(), name='filtered-inventory-list'),
     path('', InventoryListCreateView.as_view(), name='inventory-list'),
 ]
