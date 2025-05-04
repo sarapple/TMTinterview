42ffe1a16f599206f911e2f1155d9470310ec1f5
Sara <1sarabyte@gmail.com>
2025-05-04 16:20:34 -0700

    challenge 4: Added basic admin

--- interview/inventory/admin.py
+++ interview/inventory/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Inventory, InventoryTag, InventoryLanguage, InventoryType
 
 # Register your models here.
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('type', 'language', 'tags', 'metadata')
+    list_filter = ('type', 'language')
+
+admin.site.register(Inventory, InventoryAdmin)
+admin.site.register(InventoryTag)
+admin.site.register(InventoryLanguage)
+admin.site.register(InventoryType)
--- interview/order/admin.py
+++ interview/order/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Order, OrderTag
 # Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'start_date', 'embargo_date')
+    list_filter = ('start_date', 'embargo_date')
+    search_fields = ('start_date', 'embargo_date')
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderTag)
