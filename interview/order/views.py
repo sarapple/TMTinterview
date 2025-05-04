65e736348d49a9287a5b32247ff75a3434906854
Sara <1sarabyte@gmail.com>
2025-05-04 16:06:58 -0700

    challenge 3: Write an endpoint that lists orders that are between a particular start and embargo date.

--- interview/order/views.py
+++ interview/order/views.py
@@ -8,7 +8,21 @@ from interview.order.serializers import OrderSerializer, OrderTagSerializer
 class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        start_date__gte = request.query_params.get('start_date__gte')
+        embargo_date__lte = request.query_params.get('embargo_date__lte')
+        orders = self.get_queryset()
+        if start_date__gte is not None:
+            orders.filter(start_date__gte=start_date__gte)
+        if embargo_date__lte is not None:
+            orders.filter(embargo_date__lte=embargo_date__lte)
+        serializer = self.serializer_class(orders, many=True)
+        
+        return Response(serializer.data, status=200)
     
+    def get_queryset(self):
+        return self.queryset.all()
 
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
